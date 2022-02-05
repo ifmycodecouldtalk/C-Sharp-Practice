1dc724f030289c735531faa74326a1718bb8417a
Chris <93886036+ifmycodecouldtalk@users.noreply.github.com>
2022-02-05 12:41:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using project2_1;
+using static System.Console;
+
+Person sam = new(name: "Sam");
+WriteLine(sam.Origin);
+
+sam.FavoriteIceCream = "Chocolate Fudge";
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+sam.FavoritePrimaryColor = "Red";
+WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
