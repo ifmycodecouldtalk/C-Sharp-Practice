b40ce908cd29822f9e66ab9be5ab70773516722b
Chris <93886036+ifmycodecouldtalk@users.noreply.github.com>
2022-02-05 00:52:20 -0500

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2_0
+{
+    public class Person
+    {
+        public String? Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+using project_2_0;
+
+Person bob = new();
+bob.Name = "Bob";
+bob.DateOfBirth = new DateTime(1998, 06, 20);
+
+Console.WriteLine(bob.ToString());
+
+Console.WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+ arg0: bob.Name,
+ arg1: bob.DateOfBirth);
