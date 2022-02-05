95453c34f7c39b81f7225f0584877eb152251d9b
Chris <93886036+ifmycodecouldtalk@users.noreply.github.com>
2022-02-05 12:42:24 -0500

    Add files via upload

---
+++ project2_1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2_1
+{
+    public partial class Person
+    {
+        public string? Name;
+        public string HomePlanet = "Earth";
+        public DateTime DateOfBirth = DateTime.Now;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ project2_1/PersonAutoGenerated.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2_1
+{
+    public partial class Person
+    {
+        // a property defined using C# 1 - 5 syntax
+        public string Origin
+        {
+            get
+            {
+                return $"{Name} was born on {HomePlanet}";
+            }
+        }
+        // two properties defined using C# 6+ lambda expression body syntax
+        public string Greeting => $"{Name} says 'Hello!'";
+        public int Age => System.DateTime.Today.Year - DateOfBirth.Year;
+
+        // getter setter
+        public string FavoriteIceCream { get; set; }
+        // more complex getter and setter
+        private string favoritePrimaryColor;
+        public string FavoritePrimaryColor
+        {
+            get
+            {
+                return favoritePrimaryColor;
+            }
+            set
+            {
+                switch (value.ToLower())
+                {
+                    case "red":
+                    case "green":
+                    case "blue":
+                        favoritePrimaryColor = value;
+                        break;
+                    default:
+                        throw new System.ArgumentException(
+                        $"{value} is not a primary color. " +
+                        "Choose from: red, green, blue.");
+                }
+            }
+        }
+    }
+}
